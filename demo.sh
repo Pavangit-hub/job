b9a04c652ff20aab1fa418bfdf37ba181607d2fc
Pavangit-hub <pavan.devops07@gmail.com>
2023-08-18 13:18:07 +0000

    update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,3 +2,9 @@
 
 echo "***************** Display root F.S space ***********"
 df -h
+
+echo "************** Display today date ***************"
+date
+
+echo "*************** Display jenkins memory *************"
+free -m
